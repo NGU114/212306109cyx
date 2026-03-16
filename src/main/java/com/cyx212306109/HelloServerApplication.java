ec01204b9cea95edcfc3c561966fb351b8d4d128
cyx <2533074708@qq.com>
2026-03-16 16:54:04 +0800

    第二次作业

---
+++ src/main/java/com/cyx212306109/HelloServerApplication.java
@@ -0,0 +1,13 @@
+package com.cyx212306109;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServerApplication.class, args);
+    }
+
+}
